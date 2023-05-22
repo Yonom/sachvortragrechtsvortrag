bf18e69f11850e39e09f0fce00840069c6b3356d
Simon Farshid <Yonom@users.noreply.github.com>
2023-05-21 22:29:32 -0700

    Add sanitization

--- main.py
+++ main.py
@@ -6,17 +6,38 @@ import os
 
 load_dotenv()
 
-openai.api_key = os.getenv('OPENAI_API_KEY')
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 st.header("Sachvortrag vs Rechtsvortrag")
 text = st.text_area("Vertragstext")
+sanitize = st.checkbox("Sanitize text")
 
-if st.button("Analysieren"):
+analyze = st.button("Analysieren")
+
+results = []
+res_box = st.empty()
+
+
+def run_prompts_for_section(text):
+    if sanitize:
+        prompt = f"""Sanitize the following text that was scanned via OCR: 
+
+Original text:
+{text}
+
+Sanitized text:
+"""
+        chat_completion = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt,
+            max_tokens=1024,
+            temperature=0,
+        )
+        text = chat_completion.choices[0].text
+
     separator = "\n\n"
     paragraphs = text.split(separator)
 
-    results = []
-    res_box = st.empty()
     for paragraph in paragraphs:
         prompt = f"""Ist der Text ein Sachvortrag oder ein Rechtsvortrag?
 
@@ -25,6 +46,8 @@ Mögliche Antworten:
 #2. Rechtsvortrag
 #3. Beides
 #4. Keines
+
+
 
 Text: ```
 {paragraph}
@@ -51,3 +74,10 @@ Antwort: #"""
         results.append((paragraph, color_for_option))
         result = separator.join([f":{color}[{txt}]" for txt, color in results])
         res_box.markdown(result)
+
+
+if analyze:
+    separator = "\n\n\n"
+    parts = text.split(separator)
+    for part in parts:
+        run_prompts_for_section(part)
